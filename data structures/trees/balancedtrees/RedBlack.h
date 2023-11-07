87cc1fd1d5f9d46fd70933a6a9fbc6ba30bdbc8e
Ethan Gutierrez <Ethan.e.gutierrez@gmail.com>
2023-11-07 13:28:01 -0800

    adding more notes to the redblack tree

--- data structures/trees/balancedtrees/RedBlack.h
+++ data structures/trees/balancedtrees/RedBlack.h
@@ -3,6 +3,25 @@
     For comparison, the height of a binary search tree is O(n), worst case. This is because the tree can be skewed
     A Red Black Tree is a type of 2-3 Tree, which is a type of balanced search tree
 
+    Purpose: The purpose of a red-black tree is to provide a simple way to balance a BST with little overhead/complexity.
+    It accomplishes this by utilizing a simple red-black relationship between nodes with easy rotations. Its a well balanced, simple tree.
+
+    How do Red-Black trees ensure that they remain balanced? 
+        They utilize a red-black relationship between nodes that is used to determine 
+        if the tree needs to rotate nodes or recolor nodes
+
+    Properties:
+        The root of the tree must be black.
+        Every node is either red or black.
+        Every leaf (NIL or null node) is considered black.
+        If a node is red, then both its children must be black.
+        For each node, any simple path from this node to any of its descendant leaves must 
+        have the same number of black nodes. This property is sometimes called the "Black-Height" property, 
+        which ensures that the tree remains balanced.
+
+    How are rotations used in Red-Black trees to restore balance?
+        TODO
+
     Rules:
         Must be balanced
         The root must be black
